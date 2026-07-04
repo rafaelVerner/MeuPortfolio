ecbe105f752107b8106d6b6ad66d10e163417204
Verner Rafael <vernerrrferreira@gmail.com>
2026-07-03 21:59:47 -0300

    feat: Adcionando Cards

--- Portfolio/src/components/About/About.jsx
+++ Portfolio/src/components/About/About.jsx
@@ -1,13 +1,18 @@
 import './About.css';
+import catImage from '../../assets/cat.png';
 
 function About() {
     return(
         <div className="About">
-            <div className="Content">
-                <h1>Quem é Verner Rafael?</h1>
-                <p>Sou um desenvolvedor full-stack com experiência em JavaScript, React e Node.js.</p>
+            <div className="Card-Content">
+                <div className="Content">
+                    <h1>Quem sou eu?</h1>
+                    <p>Sou um desenvolvedor full-stack com experiência em JavaScript, React ee Node.js.</p>
+                </div>
             </div>
-            <div className="Image"></div>
+            <div className="Card-Image">
+                <img src={catImage} alt="Imagem de um gato" />
+            </div>
         </div>
     );
 
