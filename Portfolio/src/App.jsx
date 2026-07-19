f97a1919705173135d89a6183503964ecb432faf
Verner Rafael <vernerrrferreira@gmail.com>
2026-07-18 23:16:08 -0300

    feat: nova seção

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/About/About';
 import { profileData } from './data/profile';
 import './App.css';
 import Skills from './components/Skills/Skills';
+import Projects from './components/Projects/Projects';
 
 function App() {
   
@@ -14,7 +15,7 @@ function App() {
       <Hero Nome={profileData.nome} Profissao={profileData.profissao} Descricao={profileData.descricao} />
       <About />
       <Skills />
-
+      <Projects />
     </div>
   )
 }
---
+++ Portfolio/src/components/Projects/Projects.jsx
@@ -0,0 +1,17 @@
+import "./Projects.css";
+// import { projectsData } from "../../data/projects";
+
+function Projects() {
+    return (
+        <div className="Projects">
+            <div className="Header">
+                <h1>Meus Projetos</h1>
+            </div>
+            <div className="Projects-container">
+
+            </div>
+        </div>
+    );
+}   
+
+export default Projects;
