933bc74b6200e30187d02f8cd2ba2efee5d03b18
Verner Rafael <vernerrrferreira@gmail.com>
2026-09-04 00:08:30 -0300

    update: Novos estilos na seção de habilidades.

--- Portfolio/src/components/Skills/Skills.jsx
+++ Portfolio/src/components/Skills/Skills.jsx
@@ -8,21 +8,21 @@ function Skills() {
             <div className="Header">
                 <h1>Habilidades</h1>
             </div>
-            <div className="Skills-container">
+            <div className="Skills-Container">
                 {skillsData.map((skillData, index)=>{
                     return (
-                    <div className="skills-card" key={index}>
-                        <div className= "card-header">
+                    <div className="Skills-List" key={index}>
+                        <div className= "list-Header">
                             <h1>{skillData.type}</h1>
                         </div>
-                        <div className="card-content">
+                        <div className="Card-Content">
                                 {skillData.list.map((skill, index)=>{
                                     const IconComponent = skill.icon;
                                     return (
-                                        <div className="skills-row" key={index}>
-                                            <div className="skill-icon">{IconComponent && <IconComponent />}</div>
-                                            <div className="skill-name">{skill.name}</div>
-                                            <div className="skill-level">{skill.nivel}</div>
+                                        <div className="Skill-Card" key={index}>
+                                            <div className="Skill-Icon">{IconComponent && <IconComponent />}</div>
+                                            <div className="Skill-Name">{skill.name}</div>
+                                            <div className="Skill-Level">{skill.nivel}</div>
                                         </div>
                                     );
                                 })}
--- Portfolio/src/data/skills.js
+++ Portfolio/src/data/skills.js
@@ -1,7 +1,7 @@
 import {FaHtml5, FaCss3, FaJs, FaReact, FaPhp, FaNodeJs, FaServer, FaPython } from "react-icons/fa";
 
 export const skillsData = [
-    {   type: "Web Development", 
+    {   type: "Web", 
         list: [
             {name: "HTML", icon: FaHtml5, nivel: 5},
             {name: "CSS", icon: FaCss3, nivel: 5}, 
@@ -9,23 +9,23 @@ export const skillsData = [
             {name: "PHP", icon: FaPhp, nivel: 4}, 
             {name: "React", icon: FaReact, nivel: 3}, 
             {name: "Node.js", icon: FaNodeJs, nivel: 3}, 
-            {name: "Express.js", icon: FaServer, nivel: 3}
+            {name: "Express.js", icon: FaJs, nivel: 3}
         ]
     },
     {   type: "Data Science", 
         list: [
             {name: "Python", icon: FaPython, nivel: 4},
-            {name: "Pandas", icon: null, nivel: 3},
-            {name: "NumPy", icon: null, nivel: 3}
+            {name: "Pandas", icon: FaPython, nivel: 3},
+            {name: "NumPy", icon: FaPython, nivel: 3}
         ]
     },
     {   type: "Database", 
         list: [
-            {name: "MongoDB", icon: null, nivel: 5},
-            {name: "MySQL", icon: null, nivel: 5}
+            {name: "MongoDB", icon: FaServer, nivel: 5},
+            {name: "MySQL", icon: FaServer, nivel: 5}
         ]
     },
-    {   type: "Mobile Development", 
+    {   type: "Mobile", 
         list: [
             {name: "Dart", icon: null, nivel: 4},
             {name: "Flutter", icon: null, nivel: 4}
